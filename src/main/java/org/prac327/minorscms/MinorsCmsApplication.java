64bd1fa3665e7273a10a39d35c9da4c38aeddd3a
ytatichno <ytatichno@gmail.com>
2024-05-22 00:31:40 +0300

    Initial commit

---
+++ src/main/java/org/prac327/minorscms/MinorsCmsApplication.java
@@ -0,0 +1,13 @@
+package org.prac327.minorscms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinorsCmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MinorsCmsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/prac327/minorscms/MinorsCmsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.prac327.minorscms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinorsCmsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
