655e3b0a3558b5a71b1be175c5e6cba74899f299
ytatichno <ytatichno@gmail.com>
2024-05-24 17:56:44 +0300

    add findByCourse query in ScheduleRepository

--- src/main/java/org/prac327/minorscms/repositories/ScheduleRepository.java
+++ src/main/java/org/prac327/minorscms/repositories/ScheduleRepository.java
@@ -1,7 +1,9 @@
 package org.prac327.minorscms.repositories;
 
+import org.prac327.minorscms.models.Course;
 import org.prac327.minorscms.models.Schedule;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
@@ -12,4 +14,7 @@ import java.util.List;
 public interface ScheduleRepository extends JpaRepository<Schedule, Long> {
     List<Schedule> findByDayOfWeek(String dayOfWeek);
     List<Schedule> findByClassroom(String classroom);
+
+    @Query("SELECT s FROM Schedule s WHERE s.teach_cour.course = ?1")
+    List<Schedule> findByCourse(Course course);
 }
