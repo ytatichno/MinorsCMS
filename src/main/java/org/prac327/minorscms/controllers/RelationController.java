0897ed7243eebf5e6120afe6fd69d7a25a5dd7f2
ytatichno <ytatichno@gmail.com>
2024-05-27 15:13:05 +0300

    add create/remove rest endpoints for relation management

--- src/main/java/org/prac327/minorscms/controllers/RelationController.java
+++ src/main/java/org/prac327/minorscms/controllers/RelationController.java
@@ -11,10 +11,7 @@ import org.prac327.minorscms.repositories.TeacherRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -52,5 +49,68 @@ public class RelationController {
             return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
 
         return ResponseEntity.status(HttpStatus.FOUND).body(relation.get(0).getId());
+    }
+    @GetMapping("/s2c/{studentId}/{courseId}")
+    public ResponseEntity<?> checkS2CRelation(@PathVariable Long studentId, @PathVariable Long courseId){
+        List<Students2Courses> relation = entityManager
+                .createQuery("FROM Students2Courses s2c " +
+                                    "WHERE s2c.student.id = :studentId AND s2c.course.id = :courseId"
+                        , Students2Courses.class)
+                .setParameter("studentId", studentId)
+                .setParameter("courseId", courseId)
+                .getResultList();
+        if(relation.isEmpty() || relation.get(0) == null)
+            return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+
+        return ResponseEntity.status(HttpStatus.FOUND).body(relation.get(0).getId());
+    }
+
+
+    @PostMapping("/t2c/{teacherId}/{courseId}")
+    public ResponseEntity<?> createT2CRelation(@PathVariable Long teacherId, @PathVariable Long courseId){
+        Teachers2Courses newRelation = new Teachers2Courses(
+                teacherRepository.findById(teacherId).orElseThrow(),
+                courseRepository.findById(courseId).orElseThrow()
+        );
+        entityManager.persist(newRelation);
+//        entityManager.flush();
+        return ResponseEntity.ok(newRelation);
+    }
+    @PostMapping("/s2c/{studentId}/{courseId}")
+    public ResponseEntity<?> createS2CRelation(@PathVariable Long studentId, @PathVariable Long courseId){
+        Students2Courses newRelation = new Students2Courses(
+                studentRepository.findById(studentId).orElseThrow(),
+                courseRepository.findById(courseId).orElseThrow()
+        );
+        entityManager.persist(newRelation);
+//        entityManager.flush();
+        return ResponseEntity.ok(newRelation);
+    }
+//    @Transactional
+    @DeleteMapping("/t2c/{teacherId}/{courseId}")
+    public ResponseEntity<?> removeT2CRelation(@PathVariable Long teacherId, @PathVariable Long courseId){
+        Teachers2Courses toRemove = entityManager
+                .createQuery("FROM Teachers2Courses t2c " +
+                                "WHERE t2c.teacher.id = :teacherId AND t2c.course.id = :courseId"
+                        , Teachers2Courses.class)
+                .setParameter("teacherId", teacherId)
+                .setParameter("courseId", courseId)
+                .getSingleResult();
+        scheduleRepository.deleteAllByTeachCour(toRemove);
+        entityManager.remove(toRemove);
+        return ResponseEntity.ok(toRemove);
+    }
+//    @Transactional
+    @DeleteMapping("/s2c/{studentId}/{courseId}")
+    public ResponseEntity<?> removeS2CRelation(@PathVariable Long studentId, @PathVariable Long courseId){
+        Students2Courses toRemove = entityManager
+                .createQuery("FROM Students2Courses s2c " +
+                                "WHERE s2c.student.id = :studentId AND s2c.course.id = :courseId"
+                        , Students2Courses.class)
+                .setParameter("studentId", studentId)
+                .setParameter("courseId", courseId)
+                .getSingleResult();
+        entityManager.remove(toRemove);
+        return ResponseEntity.ok(toRemove);
     }
 }
